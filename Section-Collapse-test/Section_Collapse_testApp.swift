2177f61adf54525e021ca4bbe2d775d62a297394
Holger Krupp <email@holgerkrupp.de>
2023-05-03 08:09:26 +0200

    Initial Commit

---
+++ Section-Collapse-test/Section_Collapse_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Section_Collapse_testApp.swift
+//  Section-Collapse-test
+//
+//  Created by Holger Krupp on 03.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Section_Collapse_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
